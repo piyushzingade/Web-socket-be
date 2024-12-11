ffd7491a14d4ca19079438763cb245972f621833
Piyushzingade <piyushzingade@gmail.com>
2024-12-11 20:07:12 +0530

    Updated

--- backend-ws/src/index.ts
+++ backend-ws/src/index.ts
@@ -1,10 +1,38 @@
-import { WebSocketServer  } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
-const ws = new WebSocketServer({port: 8080})
+const ws  = new WebSocketServer({port : 3000});
+
+interface User{
+    socket : WebSocket,
+    room :string
+}
 
 
-let userCount = 0;
+let allSocket : User[] = [];
 ws.on("connection" , (socket)=>{
-    userCount++;
-    console.log("User connect $" + userCount);
-})  
+    socket.on("message" , (message)=>{
+        // @ts-ignore
+        const parseMessage = JSON.parse(message)
+
+        if(parseMessage.type ==="join"){
+            allSocket.push({
+                socket : socket,
+                room : parseMessage.payload.room
+            })
+        }
+
+        if (parseMessage.type === "chat") {
+            const currentUser = allSocket.find((x) => x.socket == socket)?.room;
+
+
+            
+            for( let i= 0;i<allSocket.length;i++){
+                if(allSocket[i].room === currentUser){
+                    allSocket[i].socket.send(parseMessage.payload.message)
+                }
+            }
+
+        }
+    })
+
+})
